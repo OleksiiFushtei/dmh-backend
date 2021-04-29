184edb0cbc47b4fe4e0b36f93442c530e1d3e090
Oleksii <46541097+OleksiiFushtei@users.noreply.github.com>
2021-04-29 19:11:21 +0200

    Update settings.py

--- dmh_backend/settings.py
+++ dmh_backend/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1gyfsuwj1@yjtdsr22@j1ratr^-sl1fu+w&25$ve1s@4nwlj-l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dmhbackend.herokuapp.com']
 
